597b8b61aaf4169f5739f4418e765666a1a4de95
klessiton <klessitom@gmail.com>
2023-10-23 06:30:19 -0300

    chore: add test description

--- encrypt-easy-client/src/controllers/text/decrypt/index.spec.ts
+++ encrypt-easy-client/src/controllers/text/decrypt/index.spec.ts
@@ -4,50 +4,51 @@ import { textDecrypt } from './index';
 beforeAll(() => {
   testEnviroment();
 });
-
-test('Should decrypt a text with success', () => {
-  const result = textDecrypt({
-    text: 'U2FsdGVkX1/mTRPkNIrP1DsGNcE+3yZxLCCvPBelTmk=',
-    password: '123',
+describe('When decrypting a text', () => {
+  test('Should decrypt a text with success', () => {
+    const result = textDecrypt({
+      text: 'U2FsdGVkX1/mTRPkNIrP1DsGNcE+3yZxLCCvPBelTmk=',
+      password: '123',
+    });
+    expect(result).toBe('TESTING');
   });
-  expect(result).toBe('TESTING');
-});
 
-test('Should not decrypt a text with wrong password', () => {
-  const result = textDecrypt({
-    text: 'U2FsdGVkX1/mTRPkNIrP1DsGNcE+3yZxLCCvPBelTmk=',
-    password: '321',
+  test('Should not decrypt a text with wrong password', () => {
+    const result = textDecrypt({
+      text: 'U2FsdGVkX1/mTRPkNIrP1DsGNcE+3yZxLCCvPBelTmk=',
+      password: '321',
+    });
+    expect(result).toBeFalsy();
   });
-  expect(result).toBeFalsy();
-});
 
-test('Should decrypt a text with success using private key', () => {
-  const result = textDecrypt({
-    text: 'U2FsdGVkX19wqB7PUbjCjugcfLmKAB/QxGui/80F+KDgWEJV/4Q023Y5kzlN2KIooUc/JdCAnv1Ox67jFYk2Cg==',
-    password: '123',
-    usePrivateKey: true,
-  });
-  expect(result).toBe('TESTING');
-});
-
-test('Should throw an error if there is not PRIVATE_KEY', () => {
-  try {
-    process.env.CLIENT_PRIVATE_KEY = '';
-    textDecrypt({
-      text: 'TESTING',
+  test('Should decrypt a text with success using private key', () => {
+    const result = textDecrypt({
+      text: 'U2FsdGVkX19wqB7PUbjCjugcfLmKAB/QxGui/80F+KDgWEJV/4Q023Y5kzlN2KIooUc/JdCAnv1Ox67jFYk2Cg==',
       password: '123',
       usePrivateKey: true,
     });
-  } catch (e) {
-    expect(e.message).toBe('privateKeyNotFound');
-  }
-  testEnviroment();
-});
+    expect(result).toBe('TESTING');
+  });
 
-test('Should return an empty string when there is not valid text or password', () => {
-  const noTextResult = textDecrypt({ text: '', password: '123' });
-  expect(noTextResult).toBeFalsy();
+  test('Should throw an error if there is not PRIVATE_KEY', () => {
+    try {
+      process.env.CLIENT_PRIVATE_KEY = '';
+      textDecrypt({
+        text: 'TESTING',
+        password: '123',
+        usePrivateKey: true,
+      });
+    } catch (e) {
+      expect(e.message).toBe('privateKeyNotFound');
+    }
+    testEnviroment();
+  });
 
-  const noPasswordResult = textDecrypt({ text: 'TESTING', password: '' });
-  expect(noPasswordResult).toBeFalsy();
+  test('Should return an empty string when there is not valid text or password', () => {
+    const noTextResult = textDecrypt({ text: '', password: '123' });
+    expect(noTextResult).toBeFalsy();
+
+    const noPasswordResult = textDecrypt({ text: 'TESTING', password: '' });
+    expect(noPasswordResult).toBeFalsy();
+  });
 });
--- encrypt-easy-client/src/controllers/text/encrypt/index.spec.ts
+++ encrypt-easy-client/src/controllers/text/encrypt/index.spec.ts
@@ -5,38 +5,40 @@ beforeAll(() => {
   testEnviroment();
 });
 
-test('Should encrypt a text with success', () => {
-  const result = textEncrypt({ text: 'TESTING', password: '123' });
-  expect(result).toBeTruthy();
-});
-
-test('Should encrypt a text with success using private key', () => {
-  const result = textEncrypt({
-    text: 'TESTING',
-    password: '123',
-    usePrivateKey: true,
+describe('When encrypting a text', () => {
+  test('Should encrypt a text with success', () => {
+    const result = textEncrypt({ text: 'TESTING', password: '123' });
+    expect(result).toBeTruthy();
   });
-  expect(result).toBeTruthy();
-});
 
-test('Should throw an error if there is not PRIVATE_KEY', () => {
-  try {
-    process.env.CLIENT_PRIVATE_KEY = '';
-    textEncrypt({
+  test('Should encrypt a text with success using private key', () => {
+    const result = textEncrypt({
       text: 'TESTING',
       password: '123',
       usePrivateKey: true,
     });
-  } catch (e) {
-    expect(e.message).toBe('privateKeyNotFound');
-  }
-  testEnviroment();
-});
+    expect(result).toBeTruthy();
+  });
 
-test('Should return an empty string when there is not valid text or password', () => {
-  const noTextResult = textEncrypt({ text: '', password: '123' });
-  expect(noTextResult).toBeFalsy();
+  test('Should throw an error if there is not PRIVATE_KEY', () => {
+    try {
+      process.env.CLIENT_PRIVATE_KEY = '';
+      textEncrypt({
+        text: 'TESTING',
+        password: '123',
+        usePrivateKey: true,
+      });
+    } catch (e) {
+      expect(e.message).toBe('privateKeyNotFound');
+    }
+    testEnviroment();
+  });
 
-  const noPasswordResult = textEncrypt({ text: 'TESTING', password: '' });
-  expect(noPasswordResult).toBeFalsy();
+  test('Should return an empty string when there is not valid text or password', () => {
+    const noTextResult = textEncrypt({ text: '', password: '123' });
+    expect(noTextResult).toBeFalsy();
+
+    const noPasswordResult = textEncrypt({ text: 'TESTING', password: '' });
+    expect(noPasswordResult).toBeFalsy();
+  });
 });
